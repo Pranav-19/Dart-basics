b2e446d8b7c70cb56375fd779e3a530f14839b7e
Pranav-19 <pranavahuja19@gmail.com>
2020-08-07 11:09:26 +0530

    initial commit

---
+++ Lecture1.dart
@@ -0,0 +1,13 @@
+
+import 'dart:io';
+main(){
+  print("Hello world");
+  var x=5;
+  var y=6;
+  print(x+y);
+  print('$x'+'$y');
+  print("Enter name");
+  String name= stdin.readLineSync();
+  print("Your name is "+name);
+
+}
